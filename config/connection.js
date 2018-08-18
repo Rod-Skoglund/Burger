00884fe1cf63b4a0549f5631d5aa3c3b4fb69b8b
RLS <rod.skoglund@gmail.com>
2018-08-18 00:08:21 -0500

    working app

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+// ************************************************************
+// Program: Eat-Da-Burger App
+// Author: Rod Skoglund
+// File: connection.js
+// ************************************************************
+
+// ************************************************************
+// Set up MySQL connection.
+// ************************************************************
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
+
+// ************************************************************
+// Make connection.
+// ************************************************************
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// ************************************************************
+// Export connection for our ORM to use.
+// ************************************************************
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- ************************************************************
+-- Program: Eat-Da-Burger App
+-- Author: Rod Skoglund
+-- File: seeds.sql
+-- ************************************************************
+
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
