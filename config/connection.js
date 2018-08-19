bfd1281151ca1c270603a13dc5a1d71189fc5f82
RLS <rod.skoglund@gmail.com>
2018-08-19 09:54:36 -0500

    added Heroku JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -9,13 +9,28 @@
 // ************************************************************
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "rootroot",
+      database: "burgers_db"
+    });
+};
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "rootroot",
+//   database: "burgers_db"
+// });
 
 // ************************************************************
 // Make connection.
